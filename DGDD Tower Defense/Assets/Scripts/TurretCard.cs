cb6db63f993bda648bc540d7a8a71cc29db9bac6
bnnmilkshake <56715685+bnnmilkshake@users.noreply.github.com>
2019-10-21 10:18:09 +0800

    towers

---
+++ DGDD Tower Defense/Assets/Scripts/TurretCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretCard : MonoBehaviour
+{
+    private LevelManager theLevelManager;
+
+    private int rarity = 1;
+    public GameObject tower;
+    public GameObject inventory;
+
+    public void Buy()
+    {
+        if(theLevelManager.currency >= rarity)
+        {
+
+        }
+    }
+}
