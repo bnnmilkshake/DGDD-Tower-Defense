66ca5c3ba877699beaf454cff58594625b644c61
merctraider <47917658+merctraider@users.noreply.github.com>
2019-10-21 10:10:12 +0800

    Added base system
    base with a health system

---
+++ DGDD Tower Defense/Assets/Scripts/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The base/end point of the player. When destroyed, the player loses.  
+/// </summary>
+
+public class BaseController : DestructibleObject
+{
+
+
+    public override void Death()
+    {
+        Debug.Log("YOU LOSE LMAO");
+    }
+}
