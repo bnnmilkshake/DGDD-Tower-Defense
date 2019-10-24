63049ad295b381ebaa29966ca53d81b1617b024e
merctraider <47917658+merctraider@users.noreply.github.com>
2019-10-24 17:43:31 +0800

    Wave System

---
+++ DGDD Tower Defense/Assets/Scripts/Wave System/Horde.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Horde 
+{
+    public GameObject[] enemyToSpawn;
+    public float timeBeforeSpawn; 
+}
